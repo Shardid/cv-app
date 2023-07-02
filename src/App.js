460c3b4bf8d9a941746d3dc21bccec76b75ddfb7
Shardid <jmsahardid47@gmail.com>
2023-07-02 19:10:21 +0100

    cv-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+const App = () => {
+  return (
+    <div className="App">
+      <section className="side-bar">
+        <button>+ New chat</button> 
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Yours truly Shards</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+        
+    </div>
+  );
+}
+
+export default App;
